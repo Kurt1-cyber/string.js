5c53d329db9a77b660b113f811a9304dd4091bd9
Kurt1-cyber <delutakurtlouie@gmail.com>
2024-09-14 22:16:56 +0800

    Create app.js

---
+++ app.js
@@ -0,0 +1,19 @@
+let favActorFirstName = 'Chris';
+let favActorLastName = 'Hemsworth';
+let fullname = favActorFirstName + ' ' + favActorLastName;
+let uppercase = fullname.toLowerCase();
+
+let petname = 'jager';
+let lowercase = 'jager'.toUpperCase();
+
+let message = 'My Favorite Actor is: ' + fullname.toUpperCase();
+message += ' his best show is Silicon Valley.';
+
+
+console.log(favActorFirstName);
+console.log(favActorLastName);
+console.log(fullname);
+console.log(uppercase);
+console.log(petname);
+console.log(lowercase);
+console.log(message);
